3bd6e52c573d8b9ae76d87942c90233d0294d94e
raghavt20 <raghavtandon2000@gmail.com>
2020-08-09 14:00:41 +0000

    sm6150-common: Nuke org.ifaa.android.manager from boot jars

--- sm6150.mk
+++ sm6150.mk
@@ -233,9 +233,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
